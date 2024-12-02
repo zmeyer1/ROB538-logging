7f11ff03cd869a11577ca747aa6954648a48ac8c
sankarip <sankarip@oregonstate.edu>
2024-12-01 17:29:53 -0800

    works

--- rover_world_3.py
+++ rover_world_3.py
@@ -2,23 +2,27 @@ import numpy as np
 import random
 import sys
 import matplotlib.pyplot as plt
+from typing import Tuple
 
-ALPHA=0.95
-GAMMA=0.4
+import a_star
+
+ALPHA=0.5
+GAMMA=0.5
 EPSILON=0.2
-COUPLING=1
-REWARD_VALUE=100
+REWARD_VALUE=300
 class newGridWorld:
-    def __init__(self,agents, size=(20, 20)):
+    def __init__(self, agents, size):
         self.agents = agents
         self.targets=agents[0].targets
+        for target in self.targets.targets_list:
+            target.visitors = [0 for _ in agents]
         self.size = size
         self.done=False
         for agent in self.agents:
             agent.world=self
             agent.size=self.size
 
-    def step(self):
+    def step(self, multiple_policies=True):
         # reset number of agents at target
         target_list = self.targets.targets_list
         for target in target_list:
@@ -38,43 +42,32 @@ class newGridWorld:
             agent.update_policy(old_locations[i][0],old_locations[i][1], directions[i], reward)
         for i, target in enumerate(target_list):
             target_location = target.location
-            for agent in self.agents:
+            for j, agent in enumerate(self.agents):
                 if agent.location == target_location:
+
                     # need to reset after each step
                     target.num_agents_present += 1
+
                     if i not in agent.trees_visited:
+                        agent.policy_location[(agent.location)] =len(agent.trees_visited)
                         agent.trees_visited.append(i)
-                        #new tree tracks if the location s new for the agent
-                        new_tree=True
-                    else:
-                        new_tree=False
-                    #go to next policy or start new one after visiting
-                    if (len(agent.policies)<=len(agent.trees_visited)) and new_tree:
-                        print('new policy created')
-                        blank_policy=agent.start_policy()
-                        agent.policies.append(blank_policy)
-                    if len(agent.policies) <=len(target_list):
-                        print('going to policy: ', len(agent.trees_visited))
-                        agent.current_policy=len(agent.trees_visited)
-                        agent.policy=agent.policies[len(agent.trees_visited)]
-                    if target.num_agents_present >= COUPLING:
-                        # trying to change value after collection
                         target.reward_value *= 0.8
-                        #target.fully_measured=True
-                        target.visits += 1
-                        if i not in target.visitors:
-                            target.visitors.append(i)
-        for i, target in enumerate(target_list):
-            if len(target.visitors)>=len(self.agents):
-                target.fully_measured=True
-        #if all are measured it's done
-        if all([t.fully_measured for t in target_list]):
+                    # trying to change value after collection
+
+                    # target.fully_measured=True
+                    target.visits += 1
+                    target.visitors[j] = 1 # unique visits
+                    if len(agent.trees_visited)<len(agent.policies):
+                        agent.policy=agent.policies[len(agent.trees_visited)]
+                    else:
+                        agent.start_policy()
+                        agent.policies.append(agent.policy)
+        if all([all(t.visitors) for t in target_list]):
             self.done = True
-
-
 
     def viz(self):
         #might be switched
+        plt.figure()
         grid = np.zeros((self.size[1], self.size[0]))
         #move_strings=np.empty((self.size[1], self.size[0]), dtype='U50')
         #only works for this many agents
@@ -86,7 +79,7 @@ class newGridWorld:
             x=agent.start_location[0]
             y=agent.start_location[1]
             plt.gca().add_patch(plt.Rectangle((x - 0.5, y - 0.5), 1, 1, color='yellow'))
-            plt.text(x,y, str(i))
+            plt.text(x-0.3,y, 'a'+str(i))
             path=agent.path
             val=0
             for entry in path:
@@ -101,7 +94,38 @@ class newGridWorld:
             else:
                 color='red'
             plt.gca().add_patch(plt.Rectangle((x - 0.5, y - 0.5), 1, 1, color=color))
+            plt.text(x-0.3, y, 'o'+str(sum(target.visitors)))
         plt.imshow(grid, cmap=cmaps[i], interpolation='nearest')
+        plt.show(block = False)
+
+    def path_viz(self):
+
+        plt.figure()
+        colors = ['b', 'm', 'r', 'y', 'g', 'l']
+        div = 0.1 # how far apart each line is
+
+        for i, agent in enumerate(self.agents):
+            color = colors[i%len(colors)]
+            path = np.array(agent.path)
+            ofst =i * div
+            plt.plot(path[0,0], path[0,1], color+'o', markersize=15)
+            plt.plot(path[:,0]+ofst, path[:,1]+ofst, colors[i%len(colors)]+"--")
+        
+        for target in self.targets.targets_list:
+            x=target.location[0]
+            y=target.location[1]
+            if target.fully_measured:
+                color='green'
+            else:
+                color='red'
+            plt.gca().add_patch(plt.Rectangle((x - 0.5, y - 0.5), 1, 1, color=color))
+            plt.text(x-0.3, y, 'o'+str(sum(target.visitors)))
+
+        plt.ylim(0,self.size[1])
+        plt.xlim(0,self.size[0])
+        plt.xticks(np.arange(-1, self.size[0], step=1))
+        plt.yticks(np.arange(-1, self.size[1], step=1))
+        plt.grid()
         plt.show()
 
 class targetsObj:
@@ -124,7 +148,6 @@ class tree:
         self.visitors = []
 
 
-
 class newAgent:
     def __init__(self, start_location, targets):
         self.policy = []
@@ -136,7 +159,9 @@ class newAgent:
         self.path=[]
         self.trees_visited=[]
         self.policies=[]
-        self.current_policy=0
+        self.policy_location={}
+
+
 
     def move(self, instruction):
         x = self.location[0]
@@ -165,14 +190,21 @@ class newAgent:
         for x in range(self.size[0]):
             for y in range(self.size[1]):
                 #up down left right
-                policy[y][x] = np.array([0.0,0.0,0.0,0.0])
-        #self.policy = policy
-        return policy
+                policy[y][x] = np.ones(4)
+                if x == 0:
+                    policy[y][x][2] = -10
+                elif x == self.size[0] - 1:
+                    policy[y][x][3] = -10
+                if y == 0:
+                    policy[y][x][0] = -10
+                elif y == self.size[1] -1:
+                    policy[y][x][1] = -10
+        self.policy = policy
 
-    def move_from_policy(self, avoid_visited=False):
+
+    def move_from_policy(self, avoid_old_locations=False):
         x = self.location[0]
         y = self.location[1]
-        #call policy y,x
         val = random.random()
         if val>EPSILON:
             direction = random.choice(np.where(self.policy[y,x]==np.max(self.policy[y, x]))[0])
@@ -180,8 +212,8 @@ class newAgent:
         else:
             direction = random.choice(range(4))
             allowed, x, y, visited = self.move(direction)
-        if avoid_visited:
-            #track allowed directions to choose from if all nearby tiles have been visited
+        #track allowed directions to choose from if all nearby tiles have been visited
+        if avoid_old_locations:
             allowed_moves=[]
             bad_move_count=0
             #try other directions if not allowed
@@ -213,9 +245,13 @@ class newAgent:
         y = self.location[1]
         reward=0
         for i, target in enumerate(self.targets.targets_list):
-            if target.location == (x,y) and i not in self.trees_visited:  #and target.num_agents_present>=COUPLING
-                #not using coupling+ would have to change the order of establishing num agents present and getting reward
+            # if target.location == (x,y):
+            #     print('coupling ', target.num_agents_present>=COUPLING)
+            #     print('visited: ', i not in self.trees_visited)
+            if target.location == (x,y) and i not in self.trees_visited:
                 reward+=target.reward_value
+            else:
+                reward+=0
         return reward
 
     #check reward at next location without actually changing x,y
@@ -223,68 +259,72 @@ class newAgent:
     def get_reward_location(self,x,y):
         reward=0
         for target in self.targets.targets_list :
-            if target.location == (x,y) and target.num_agents_present>=COUPLING:
+            if target.location == (x,y):
                 reward+=target.reward_value
             else:
                 reward+=0
         return reward
 
+    def find_next_position(self, x, y, direction):
+        # directions are: up, down, left, right, but -y is up
+        if direction == 0:
+            y -= 1
+        elif direction == 1:
+            y += 1
+        elif direction == 2:
+            x -= 1
+        elif direction == 3:
+            x += 1
+        return x,y
+
+
     def update_policy(self,x,y,direction, reward):
-        self.policy[y,x][direction] = reward
+        x_next, y_next = self.find_next_position(x,y,direction)
+        if reward > 0:
+            self.policy[y,x][direction] += reward
+            self.propogate_reward_along_path()
+        else:
+            # reward is empty
+            self.policy[y][x][direction] += ALPHA * (GAMMA*max(self.policy[y_next,x_next]) - self.policy[y,x][direction])
+
+    def propogate_reward_along_path(self):
+        # uses A* to move along best path, starting at current location
+        path = a_star.a_star_search(np.zeros(self.size), self.location, self.start_location)
+        if path is None:
+            return
+        x_next, y_next = path[0]
+        for x,y in path[1:]:
+            if x_next == x:
+                direction = 0 if y_next < y else 1
+            elif y_next == y:
+                direction = 2 if x_next < x else 3
+            else:
+                print(f"Path moved strangely to {x_next,y_next} from {x,y}!")
+                exit(1)
+            reward = self.policy[y,x][direction] + ALPHA * (GAMMA*max(self.policy[y_next,x_next]) - self.policy[y,x][direction])
+            self.policy[y][x][direction]=reward
+            x_next, y_next = x,y
+            
 
     def propagate_reward(self):
-        #only propogates on most recently policy (maybe good?)
         for x in range(self.size[0]):
             for y in range(self.size[1]):
                 for direction in range(4):
-                    new_y=y
-                    new_x=x
-                    if direction == 0:
-                        new_y -= 1
-                    if direction == 1:
-                        new_y += 1
-                    if direction == 2:
-                        new_x -= 1
-                    if direction == 3:
-                        new_x += 1
+                    new_x, new_y = self.find_next_position(x,y, direction)
                     #check if possible move
                     if new_x >= 0 and new_x < self.size[0] and new_y >= 0 and new_y < self.size[1]:
-                        #reward=self.policy[y,x][direction]+ALPHA*(GAMMA*max(self.policy[new_y,new_x])-self.policy[y,x][direction])
-                        reward = max(self.policy[new_y,new_x])*.9
-                        reward=round(reward,2)
-                        #print(f"Qsa: {self.policy[y,x][direction]}  best possible reward: {max(self.policy[new_y, new_x])} final reward: {reward}")
-                    #bad reward if trying to run off map
-                    else:
-                        reward=0
-                    self.policy[y][x][direction]=reward
-
-        #above goes from top left to bottom right, need opposite as well
-        for x in range(self.size[0]):
-            for y in range(self.size[1]):
-                for direction in range(4):
-                    new_y=self.size[1]-1-y
-                    new_x=self.size[0]-1-x
-                    if direction == 0:
-                        new_y -= 1
-                    if direction == 1:
-                        new_y += 1
-                    if direction == 2:
-                        new_x -= 1
-                    if direction == 3:
-                        new_x += 1
-                    #check if possible move
-                    if new_x >= 0 and new_x < self.size[0] and new_y >= 0 and new_y < self.size[1]:
-                        #reward=self.policy[y,x][direction]+ALPHA*(GAMMA*max(self.policy[new_y,new_x])-self.policy[y,x][direction])
-                        reward = max(self.policy[new_y, new_x]) * .9
-                        reward=round(reward,2)
+                        reward=self.policy[y,x][direction]+ALPHA*(GAMMA*max(self.policy[new_y,new_x])-self.policy[y,x][direction])
+                        reward=round(reward,4)
                         #print(f"Qsa: {self.policy[y,x][direction]} reward at next step: {self.get_reward_location(new_x,new_y)} best possible reward: {max(self.policy[new_y, new_x])} final reward: {reward}")
                     #bad reward if trying to run off map
                     else:
-                        reward=0
-                    self.policy[self.size[1]-1-y][self.size[0]-1-x][direction]=reward
+                        reward=-10
 
-def policy_translation(policy, agent, starting_location_focused=False):
-    agent_policy=policy.copy()
+                    self.policy[y][x][direction]=reward
+
+
+def policy_translation(agent, starting_location_focused=False):
+    agent_policy=agent.policy.copy()
     start_location=agent.start_location
     targets=agent.targets.targets_list.copy()
     target_locations=[]
@@ -314,36 +354,77 @@ def policy_translation(policy, agent, starting_location_focused=False):
                 translated_policy[y, x] +=' '
     print(translated_policy)
 
-#twoAgents()
-#one_agent()
-#system_level_reward()
+
+def plot_policy(agent, policy_index):
+    plt.figure()
+    policy=agent.policies[policy_index]
+    max_policy = np.zeros_like(policy, dtype=np.float32)
+    for x in range(agent.size[0]):
+            for y in range(agent.size[1]):
+                max_policy[y,x] = max(policy[y,x])
+                directions=np.where(policy[y,x]==max(policy[y,x]))[0]
+                delta_x = 0
+                delta_y = 0
+                for direction in directions:
+                    if direction==0:
+                        plt.arrow(x, y-0.2, 0, -1, shape='full', lw=0, color='k', length_includes_head=True, head_width=.25, head_starts_at_zero=True)
+                    elif direction==1:
+                        plt.arrow(x, y+0.2, 0, 1, shape='full', lw=0, color='k', length_includes_head=True, head_width=.25, head_starts_at_zero=True)
+                    elif direction==2:
+                        plt.arrow(x-0.2, y, -1, 0, shape='full', lw=0, color='k', length_includes_head=True, head_width=.25, head_starts_at_zero=True)
+                    elif direction==3:
+                        plt.arrow(x+0.2, y, 1, 0, shape='full', lw=0, color='k', length_includes_head=True, head_width=.25, head_starts_at_zero=True)
+    print(np.max(max_policy))
+    print(np.min(max_policy))
+    for target in agent.targets.targets_list:
+        plt.plot(target.location[0], target.location[1], 'ro', markersize=8)
+    plt.imshow(max_policy, cmap='Purples', interpolation='nearest')
+    plt.title('Agent Policy')
+    plt.show()
+
+    
+
+
+def generate_random_point(grid_size: Tuple[int])-> Tuple[int]:
+    """
+    Selects a random position within the grid of size grid_size
+    """
+    assert len(grid_size) == 2
+    return random.choice(range(grid_size[0])), random.choice(range(grid_size[1]))
+
+
+
 def testing(eps):
-    targets=[(7,7), (15,15)] #,, (8,8)
-    targets=targetsObj(targets)
-    agent1=newAgent((3,2),targets)
-    #agent2 = newAgent((4, 4), targets)
-    #agent3 = newAgent((5, 4), targets)
-    gWorld = newGridWorld([agent1]) #, agent2, agent3
+    # keeping this here so we can use it to determine random points
+    grid_size = (30,30)
+
+    num_targets = 5
+    # two targets could be at the same location....eh, as grid size expands, this won't be likely
+    targets=targetsObj([(grid_size[0]//num_targets*i, grid_size[1]//num_targets*i) for i in range(num_targets)])
+
+    num_agents = 3
+    agents = [newAgent((random.choice(range(15,20)), random.choice(range(0,5))), targets) for _ in range(num_agents)]
+
+    gWorld = newGridWorld(agents, grid_size)
     for agent in gWorld.agents:
-        #start with a blank policy
-        agent.policy=agent.start_policy()
+        agent.start_policy()
         agent.policies.append(agent.policy)
-    iterations=300
-    epochs=100
+    iterations=200
+    epochs=1000
     for epoch in range(epochs):
         #reset path traveled and it tree is measured every epoch
         epsilon = eps
         for agent in gWorld.agents:
             agent.path=[]
-            agent.location=agent.start_location
-            agent.trees_visited=[]
+            # reset to a new random position
+            agent.start_location = (random.choice(range(15,20)), random.choice(range(0,5)))
+            agent.location = agent.start_location
             agent.policy=agent.policies[0]
-            agent.current_policy=0
-            #for t in agent.targets.targets_list:
-                #print(t.fully_measured)
+            agent.trees_visited=[]
         for target in targets.targets_list:
             target.fully_measured=False
-            target.visitors=[]
+            target.visits=0
+            target.visitors=[0 for _ in gWorld.agents]
             target.reward_value=REWARD_VALUE
         gWorld.done=False
         for iteration in range(iterations):
@@ -353,27 +434,20 @@ def testing(eps):
             if gWorld.done:
                 print(f"moves to solve: {iteration}")
                 break
-        #policy_translation(agent1, True)
-        if epoch%10==0:
-            gWorld.viz()
+        #policy_translation(agents[0], True)
+        # if epoch%10==0:
+            # gWorld.viz()
         #green: visited target, red: unvisited target, blue: path increasing darkness withtime, yellow: start location w/agent number
-        for agent in gWorld.agents:
-            if gWorld.done:
-                # print('current policy: ',agent.current_policy)
-                # print(agent.policy)
-                agent.propagate_reward()
-                # print('after prop: ')
-                # print(agent.policy)
-
-    print(len(agent1.policies))
-    for policy in agent1.policies:
-        policy_translation(policy,agent1, True)
 
 
-testing(EPSILON)
-#need to explore then propogate rewards with q learning
-#policy collapses with too many iterations
-#something with back prop rule, just goes to 0
-#only good for 10 epochs
-#bad paths caused by deviation with e greedy, then no retrace goes randomly exploring
-#make propogation sweep around
+    gWorld.path_viz()
+    plot_policy(agents[0],0)
+    plot_policy(agents[0], 1)
+    plot_policy(agents[0],2)
+    plot_policy(agents[0], 3)
+    policy_translation(agents[0], True)
+
+
+if __name__ == "__main__":
+    testing(EPSILON)
+    #need to explore then propogate rewards with q learning
